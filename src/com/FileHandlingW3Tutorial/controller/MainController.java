bf64357d0387a1155458005055d07006d43645f2
omnisilica <oxmaverix@gmail.com>
2022-08-20 22:39:59 -0400

    Added readFile(File file) method to read contents of a file.

--- src/com/FileHandlingW3Tutorial/controller/MainController.java
+++ src/com/FileHandlingW3Tutorial/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.FileHandlingW3Tutorial.controller;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,31 +14,68 @@ public class MainController {
 	public static void main(String[] args) {
 		try {
 			String baseDirectory = System.getProperty("user.dir");
+			logger.log(Level.INFO, baseDirectory + ".\n");
+			
 			String fileName = "filename.txt";
-			
-			logger.log(Level.INFO, baseDirectory);
-			
+						
 			String fileDestination = baseDirectory + "\\src\\resources\\";
-			logger.log(Level.INFO, fileDestination);
-			logger.log(Level.INFO, fileDestination+fileName);
+			logger.log(Level.INFO, fileDestination + ".\n");
+			logger.log(Level.INFO, fileDestination + fileName + ".\n");
 			
 			File directory = new File(fileDestination);
-			File objectFile = new File(fileDestination + fileName);
 			
 			if(!directory.exists()) {
 				directory.mkdir();
 			}
 			
+			String fileNamePath = fileDestination + fileName;
+			
+			File objectFile = new File(fileNamePath);
+			
 			if(objectFile.createNewFile()) {
-				System.out.println("File created: " + objectFile.getName());
+				logger.log(Level.INFO, "File created: " + objectFile.getName() + ".\n");
 			} else {
-				System.out.println("File already exists.");
+				logger.log(Level.INFO, "File already exists." + "\n");
 			}
+			
+			writeToFile(fileNamePath, "File content.");
+			
+			readFile(objectFile);
 		} catch(IOException e) {
-			System.out.println("Something went wrong");
+			logger.log(Level.WARNING, "Something went wrong." + "\n");
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static void writeToFile(String fileName, String fileContent) {
+		
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+			fileWriter.write(fileContent);
+			fileWriter.close();
+			
+		} catch(IOException e) {
+			logger.log(Level.WARNING, "Could not write to file." + "\n");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void readFile(File file) {
+		try {
+			Scanner fileReader = new Scanner(file);
+			
+			while(fileReader.hasNextLine()) {
+				String fileLine = fileReader.nextLine();
+				System.out.println(fileLine);
+			}
+			
+			fileReader.close();
+		} catch(IOException e) {
+			logger.log(Level.WARNING, "Could not read file.\n");
+			e.printStackTrace();
+		}
+		
 	}
 
 }
